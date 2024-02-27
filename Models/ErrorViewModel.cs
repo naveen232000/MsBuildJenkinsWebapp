c6c446b320cf58d96e4fda8e04ade45bcece3294
mygit@23 <naveenmadhesan23@gmail.com>
2024-02-27 16:05:45 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace msbuildjenkin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
